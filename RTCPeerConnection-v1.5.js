1c76ce55ccf5ba8f886ce90eb3a47d53616040e5
iamsagarvarma <32607188+iamsagarvarma@users.noreply.github.com>
2021-05-07 11:23:27 +0530

    Update RTCPeerConnection-v1.5.js
    Added turn server

--- RTCPeerConnection-v1.5.js
+++ RTCPeerConnection-v1.5.js
@@ -268,10 +268,26 @@ var IceServersHandler = (function() {
     function getIceServers(connection) {
         // resiprocate: 3344+4433
         // pions: 7575
-        var iceServers = [];
+        var iceServers = [
+        {
+                'urls': [
+                    'turn:14.141.53.98:3478',
+                    'turn:14.141.53.98:3478?transport=tcp'
+                ],
+                'username': 'test',
+                'credential': 'test1234#%'
+            }
+        ];
 
         if (typeof window.InstallTrigger !== 'undefined') {
-            iceServers = [];
+            iceServers = [{
+                'urls': [
+                    'turn:14.141.53.98:3478',
+                    'turn:14.141.53.98:3478?transport=tcp'
+                ],
+                'username': 'test',
+                'credential': 'test1234#%'
+            }];
         }
 
         return iceServers;
